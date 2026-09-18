4e3413c7307f88194ecdb81a6c603cdfcae7ba6d
Thanos Kalfopoulos <85345448+tkalfo21@users.noreply.github.com>
2026-09-18 18:53:26 +0200

    Update BookingTests.java

--- src/test/java/BookingTests.java
+++ src/test/java/BookingTests.java
@@ -1,34 +1,35 @@
+import ch.bbw.Booking;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.*;
 
-/**
- * Tests für die Klasse Booking.
- *
- * @author Luigi Cavuoti
- * @version 1.1
- */
 public class BookingTests
 {
-	/**
-	 * Tests f�r die Erzeugung von Buchungen.
-	 */
-	@Test
-	@DisplayName("Construct Simple Booking, should work ")
-	public void testInitialization()
-	{
-		fail("ToDo");
-	}
+    @Test
+    @DisplayName("Construct Simple Booking, should work")
+    public void testInitialization()
+    {
+        Booking booking = new Booking(1000L, 5000L, "Einzahlung");
 
-	/**
-	 * Experimente mit print().
-	 */
-	@Test
-	@DisplayName("Not a real test, it prints a list of bookings")
-	public void testPrint()
-	{
-		fail("ToDo");
-	}
+        assertEquals(1000L, booking.date());
+        assertEquals(5000L, booking.amount());
+        assertEquals("Einzahlung", booking.text());
+    }
+
+    @Test
+    @DisplayName("Not a real test, it prints a list of bookings")
+    public void testPrint()
+    {
+        Booking booking1 = new Booking(1000L, 5000L, "Einzahlung");
+        Booking booking2 = new Booking(2000L, -2000L, "Auszahlung");
+
+        List<Booking> bookings = List.of(booking1, booking2);
+
+        bookings.forEach(System.out::println);
+
+        assertEquals(2, bookings.size());
+    }
 }
